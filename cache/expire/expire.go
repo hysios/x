c6e4eb25702b5ecfd041d2db0bebd8129c17df9a
Hysios Hu <hysios@gmail.com>
2024-05-31 20:02:00 +0800

    Feature: add expire cache

---
+++ cache/expire/expire.go
@@ -0,0 +1,39 @@
+package expire
+
+import (
+	"time"
+
+	"github.com/hashicorp/golang-lru/v2/expirable"
+	"github.com/hysios/x/cache"
+)
+
+type expireCache[Key comparable, Value any] struct {
+	cache *expirable.LRU[Key, Value]
+}
+
+func New[Key comparable, Value any](size int, m expirable.EvictCallback[Key, Value], ttl time.Duration) cache.Cache[Key, Value] {
+	l := expirable.NewLRU[Key, Value](size, m, ttl)
+
+	return &expireCache[Key, Value]{
+		cache: l,
+	}
+}
+
+// Load returns the value stored in the cache for a key, or nil if no value is present.
+func (l *expireCache[Key, Value]) Load(key Key, opts ...cache.LoadOpt) (val Value, ok bool) {
+	return l.cache.Get(key)
+}
+
+// Update sets the value for a key.
+func (l *expireCache[Key, Value]) Update(key Key, val Value, opts ...cache.UpdateOpt) {
+	l.cache.Add(key, val)
+}
+
+// Clear removes the value for a key.
+func (l *expireCache[Key, Value]) Clear(key Key) {
+	l.cache.Remove(key)
+}
+
+func (l *expireCache[Key, Value]) Keys() []Key {
+	return l.cache.Keys()
+}
