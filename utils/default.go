4c95b452671be69f349c80d7acfdabf0bf5ae889
Hysios Hu <hysios@gmail.com>
2024-06-02 21:11:27 +0800

    Feature: add DefaultSlice func

--- utils/default.go
+++ utils/default.go
@@ -1,10 +1,25 @@
 package utils
 
-// Default
-func Default[T comparable](val T, def T) T {
+func Default[T comparable](val T, def ...T) T {
 	var z T
 	if val == z {
-		return def
+		for _, d := range def {
+			if d != z {
+				return d
+			}
+		}
+	}
+	return val
+}
+
+// DefaultSlice
+func DefaultSlice[T any](val []T, def ...[]T) []T {
+	if len(val) == 0 {
+		for _, d := range def {
+			if len(d) > 0 {
+				return d
+			}
+		}
 	}
 	return val
 }
